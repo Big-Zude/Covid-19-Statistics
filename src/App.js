9e8eb1e5cddcbf2b669811713aa1426bb7fc4f7e
Zude <zudemwango@gmail.com>
2020-03-22 12:06:29 +0200

    imported the card component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import WorldTable from './components/WorldTable';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Cards/>
      <WorldTable/>
     </div>
   );
